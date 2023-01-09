2ebcf65659672363a75a3708ec19d4970ab6245f
soohykeee <gur1028@skuniv.ac.kr>
2023-01-09 15:49:47 +0900

    파일 업로드 섬네일 처리 + 삭제 처리

--- src/main/java/com/example/moviereview/controller/UploadController.java
+++ src/main/java/com/example/moviereview/controller/UploadController.java
@@ -2,6 +2,7 @@ package com.example.moviereview.controller;
 
 import com.example.moviereview.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -62,7 +64,14 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try {
+                // 원본 파일 저장
                 uploadFile.transferTo(savePath);
+
+                //섬네일 생성 - 섬네일은 s로 시작하도록 저장
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator + "s_" + uuid + "_" + fileName;
+                File thumbnailFile = new File(thumbnailSaveName);
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
+
                 resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
@@ -110,5 +119,26 @@ public class UploadController {
         }
 
         return folderPath;
+    }
+
+
+    @PostMapping("/removeFile")
+    public ResponseEntity<Boolean> removeFile(String fileName) {
+        String srcFileName = null;
+
+        try {
+            srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            File file = new File(uploadPath + File.separator + srcFileName);
+            boolean result = file.delete();
+
+            File thumbnail = new File(file.getParent(), "s_" + file.getName());
+
+            result = thumbnail.delete();
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
